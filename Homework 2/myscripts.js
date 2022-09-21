1fb17b7df02d3c5a67737918c6277068752fa2d1
Artur Zhemchuzhnikov <58139134+artzhemch@users.noreply.github.com>
2022-09-21 23:57:15 +0300

    Add files via upload
    Простенькое экспериментирование с HTML, CSS и Java Script. Изначально хотел выводить случайную картинку из Гугла по соответсвующему запросу, но не смог реализовать.

---
+++ Homework 2/myscripts.js
@@ -0,0 +1,12 @@
+function requestImage() {
+      //alert(111)
+      
+      var rad = document.getElementsByName("animal");
+      for (var i = 0; i < rad.length; i++) {
+            if (rad[i].checked) {
+                  //alert('Chosen "' + rad[i].value + '" requesting from server...');
+                  document.getElementById("commentary").innerText = "Chose " + rad[i].value
+            }
+      } 
+}
+
