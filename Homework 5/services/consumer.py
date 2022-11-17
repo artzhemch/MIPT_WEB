900d54efd0443feb15ea8d9b6aa8af2fe47ba1b5
Artur Zhemchuzhnikov <58139134+artzhemch@users.noreply.github.com>
2022-11-17 14:59:06 +0300

    Add files via upload
    consumer, producer на Питоне

---
+++ Homework 5/services/consumer.py
@@ -0,0 +1,17 @@
+from kafka import KafkaConsumer
+import time
+import redis
+
+from paramaters import REDIS_PORT, KAFKA_PORT, SERVER_PORT, WAIT_TIME
+# Порт Редиса 6379 (такой же как в http-task)
+
+
+pool = redis.ConnectionPool(host='localhost', port=REDIS_PORT, db=0)
+consumer = KafkaConsumer('task_queue')
+redis = redis.Redis(connection_pool=pool)
+
+for message in consumer:
+    print(message)
+    redis.set('task "{message}"', 'accepted')
+    time.sleep(WAIT_TIME)
+    redis.set('task "{message}"', 'done')
---
+++ Homework 5/services/producer.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from kafka import KafkaProducer
+import redis
+
+from paramaters import REDIS_PORT, KAFKA_PORT, SERVER_PORT
+# Порт Кафки 9092, Редиса 6379, HTTP Сервера 8901
+
+
+producer = KafkaProducer(bootstrap_servers=f'localhost:{KAFKA_PORT}')
+pool = redis.ConnectionPool(host='localhost', port=REDIS_PORT, db=0)
+redis = redis.Redis(connection_pool=pool)
+
+class HttpProcessor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        request = self.requestline
+        task_no = request.split()[1]
+        redis.set(f'task{task_no} in_queue')
+        producer.send('task_queue', b'task_no')  # b-строка, потому что требуется в битовом виде
+        self.send_response(200)
+        self.send_header('content-type','text/html')
+        self.end_headers()
+        self.wfile.write(b"Inazuma shines eternal!")
+
+
+def main():
+    serv = HTTPServer(("localhost", SERVER_PORT), HttpProcessor)
+    serv.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
+    
