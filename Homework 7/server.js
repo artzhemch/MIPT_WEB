2a794faabe0e3a34be98516967206d18aa19b6a9
Artur Zhemchuzhnikov <58139134+artzhemch@users.noreply.github.com>
2022-12-08 00:55:02 +0300

    Add files via upload

--- Homework 7/server.js
+++ Homework 7/server.js
@@ -1,17 +1,38 @@
-const https = require('https');
-const fs = require('fs');
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      path = require("path"),
+      webpush = require("web-push");
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+const vapidKeys = webpush.generateVAPIDKeys(),
+      port = 1234,
+      mail = "artzhemch",
+      publicKey = "MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAa5IKqT7m4KcJ0np38M1Jaa25HLYYDJuXr+O0YfmDzLrKidFOaXBKgEqrLuomZMBQ6SIeg/aPv2j6QqbFXi6+0QIDAQAB",
+      privateKey = "MIIBOAIBAAJAa5IKqT7m4KcJ0np38M1Jaa25HLYYDJuXr+O0YfmDzLrKidFOaXBK\
+      gEqrLuomZMBQ6SIeg/aPv2j6QqbFXi6+0QIDAQABAkAFvxZ8thSFWccHjG1N2tma\
+      IKKr6vpb6g/vYH2pYjftPtdHQCeK9k7UKS19ISAwhPygshcGehroKw77e88WPvyx\
+      AiEAxPz6M8KKbQEbqSuExcpMZH+sCaiN5yq1bjRDmK0gKAUCIQCLy57Qt5SkWrH+\
+      s0hTGgmqoBqgVRMBL0ihu9kUHl5xXQIgLj8eFmzDxteNwOegojbePHHk19ekiPLz\
+      6U2H2R48AGkCIA5KptXXrs6OpxvO4Fn+k3ZqH868Y9D/MIG4Xpz77dPRAiBNVUqk\
+      22JvZkFLK/OKsINjXsqPGkLvesCaolswUOyCHg==";
 
-fs.readFile('./index.html',
-    function(error, html) {
-        if (error) throw error;
-        https.createServer(options, function (req, res) {
-            res.writeHead(200);
-            res.write(html);
-            res.end();
-    }).listen(8000)
-})
+webpush.setVapidDetails("mailto:" + mail, publicKey, privateKey);
+const app = express();
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.post("/sendpush", (req, res) => {
+  res.status(201).json({}); 
+  webpush.sendNotification(req.body, JSON.stringify({
+    title: "Notification",
+    body: "Text"
+  }))
+  .catch((err) => { console.log(err); });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+});
